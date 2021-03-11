18d28d28953c361706b344b02d63cbe3d5bd695a
dcordobag <32066181+dcordobag@users.noreply.github.com>
2021-03-11 12:15:46 -0500

    ok firma

---
+++ WPFCCPereira/WPFCCPereira/UserControls/Renewal/DigitalSignatureUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCCPereira.UserControls.Renewal
+{
+    /// <summary>
+    /// Interaction logic for DigitalSignatureUC.xaml
+    /// </summary>
+    public partial class DigitalSignatureUC : UserControl
+    {
+        public DigitalSignatureUC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReturn_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        private void btnNext_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+    }
+}
