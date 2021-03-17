e375dd782c02300a3e5bbb23b24ec3bcbdb26cbb
dcordobag <32066181+dcordobag@users.noreply.github.com>
2021-03-17 10:02:57 -0500

    ok

--- WPFCCPereira/WPFCCPereira/UserControls/Renewal/FormsPpal/UbicacionDatosGeneralesUC.xaml.cs
+++ WPFCCPereira/WPFCCPereira/UserControls/Renewal/FormsPpal/UbicacionDatosGeneralesUC.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Reflection;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WPFCCPereira.Classes;
 using WPFCCPereira.Models;
 using WPFCCPereira.Resources;
@@ -44,11 +35,44 @@ namespace WPFCCPereira.UserControls.Renewal.FormsPpal
         {
             try
             {
-              
-
-                //transaction.ExpedientesMercantil.idclaseName = typeIdentification;
+                
 
                 this.DataContext = transaction;
+            }
+            catch (Exception ex)
+            {
+                Error.SaveLogError(MethodBase.GetCurrentMethod().Name, this.GetType().Name, ex, MessageResource.StandarError);
+            }
+        }
+
+        private void ChangeView(int tag)
+        {
+            try
+            {
+                if (tag == 1)
+                {
+                    btnUbicacionComercial.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffff"));
+                    btnUbicacionComercial.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#014c6c"));
+                    
+                    btnUbicacionNotificacion.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#014c6c"));
+                    btnUbicacionNotificacion.Background = new SolidColorBrush(Color.FromArgb(127,System.Drawing.Color.Black.R, System.Drawing.Color.Black.G, System.Drawing.Color.Black.B));
+
+                    grvNotificacion.Visibility = Visibility.Hidden;
+                    grvComercial.Visibility = Visibility.Visible;
+                    grvComercial2.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    btnUbicacionNotificacion.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffff"));
+                    btnUbicacionNotificacion.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#014c6c"));
+
+                    btnUbicacionComercial.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#014c6c"));
+                    btnUbicacionComercial.Background = new SolidColorBrush(Color.FromArgb(127, System.Drawing.Color.Black.R, System.Drawing.Color.Black.G, System.Drawing.Color.Black.B));
+
+                    grvNotificacion.Visibility = Visibility.Visible;
+                    grvComercial.Visibility = Visibility.Hidden;
+                    grvComercial2.Visibility = Visibility.Hidden;
+                }
             }
             catch (Exception ex)
             {
@@ -66,6 +90,21 @@ namespace WPFCCPereira.UserControls.Renewal.FormsPpal
         private void btnReturn_TouchDown(object sender, TouchEventArgs e)
         {
             Utilities.navigator.Navigate(UserControlView.Ppal_Identificacion, data: transaction);
+        }
+
+        private void btnUbicacion_TouchDown(object sender, TouchEventArgs e)
+        {
+            ChangeView(Convert.ToInt32((sender as TextBlock).Tag));
+        }
+
+        private void TextAlfaNumerico_TouchDown(object sender, TouchEventArgs e)
+        {
+            Utilities.OpenKeyboard(false, sender, this);
+        }
+        
+        private void TextNumerico_TouchDown(object sender, TouchEventArgs e)
+        {
+            Utilities.OpenKeyboard(true, sender, this);
         }
         #endregion
     }
