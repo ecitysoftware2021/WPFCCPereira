6003d7128a04f85d87d8d2735603781b483e2249
Lian Rave <julianrivera207215@correo.itm.edu.co>
2021-04-16 10:00:55 -0500

    OK

--- WPFCCPereira/WPFCCPereira/UserControls/Renewal/FormsPpal/InformacionFinancieraUC.xaml.cs
+++ WPFCCPereira/WPFCCPereira/UserControls/Renewal/FormsPpal/InformacionFinancieraUC.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -41,6 +42,41 @@ namespace WPFCCPereira.UserControls.Renewal.FormsPpal
         private void btnNext_TouchDown(object sender, TouchEventArgs e)
         {
             Utilities.navigator.Navigate(UserControlView.Ppal_SistemaSeguridad, data: transaction);
+        }
+
+        private void NuevosActivos_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                TextBox text = (TextBox)sender;
+
+                if (text.Text.Length > 13)
+                {
+                    text.Text = text.Text.Remove(text.Text.Length - 1);
+                }
+
+                //if (text.Tag.ToString() == "0")
+                //{
+                //    txtErrorActivos.Text = string.Empty;
+                //    bdrActivos.BorderBrush = new SolidColorBrush(Color.FromArgb(0x00, 0xFF, 0xFF, 0xFF));
+                //}
+                //else
+                //{
+                //    var service = text.DataContext as ListEstablecimientos;
+
+                //    service.bdActivos = "Transparent";
+                //    service.mserroractivos = string.Empty;
+                //}
+            }
+            catch (Exception ex)
+            {
+                Error.SaveLogError(MethodBase.GetCurrentMethod().Name, this.GetType().Name, ex, ex.ToString());
+            }
+        }
+
+        private void NuevosActivos_TouchDown(object sender, TouchEventArgs e)
+        {
+            Utilities.OpenKeyboard(true, sender as TextBox, this);
         }
     }
 }
